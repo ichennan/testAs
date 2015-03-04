73f53d6e30c3788134aad8ebb69704efb01e46cd
terry.chen <ichennan@live.com>
2015-03-04 15:53:52 +0800

    learnt how to use git

---
+++ app/src/androidTest/java/com/terryc7/as7terry/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.terryc7.as7terry;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
